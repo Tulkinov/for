2ac0b6ba94cd722faa560b3b93109d058171d224
Mac <mac@makkunoMacBook-Pro.local>
2021-07-15 20:23:40 +0900

    for

---
+++ for.js
@@ -0,0 +1,28 @@
+
+// 10dan 0gacah
+
+// for(let i=10; i>0; i--){
+//     console.log(i);
+// }
+
+
+// toq son juft son
+
+
+// for(let i= 0; i<=10; i++){
+//     if (i%2 === 0) {
+//         console.log(i+` juft son`);
+//     }else{
+//         console.log(i+` toq son`);
+//     }
+// }
+
+
+// xam 5ga xam 3ga bolinadigan sonlar
+
+
+for(let i= 1; i<=100; i++){
+    if (i%15=== 0) {
+        console.log(i);
+    }
+}
